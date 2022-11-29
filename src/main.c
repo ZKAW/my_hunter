3a189b2762526e5759f4c32876db3d17d30a9869
ZKAW <zkaw.exod@gmail.com>
2022-11-29 18:14:42 +0100

    feat(display): help page

--- src/main.c
+++ src/main.c
@@ -6,8 +6,25 @@
 */
 
 #include "../include/myhunter.h"
+#include "../include/my.h"
 
-int main(void)
+void display_help(void)
 {
+    my_putstr("DESCRIPTION:\n");
+    my_putstr("\tInspired by the famous \"Duck Hunt\" game.\n");
+    my_putstr("\tYou have to shoot the Suk before they reach the end");
+    my_putstr(" of the screen.\n");
+    my_putstr("\tYou have 3 lives.\n");
+    my_putstr("INPUT:\n");
+    my_putstr("\tMove the crosshair with the mouse.\n");
+    my_putstr("\tLeft click to shoot.\n");
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2 && av[1][0] == '-' && av[1][1] == 'h') {
+        display_help();
+        return (0);
+    }
     return (my_hunter());
 }
